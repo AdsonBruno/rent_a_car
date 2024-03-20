e3745a7b965dff314ddf8dd22b47af22430b3e69
AdsonBruno <adsonbruno2@gmail.com>
2024-03-20 01:06:08 -0300

    chore: starting application

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rental of Vehicle
+
+Este projeto tem como intuito criar uma aplicação para o TCC utilizando a arquitetura Model-View-Controller (MVC)
---
+++ android/app/src/main/kotlin/br/com/rentacar/rental_of_vehicle/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.rentacar.rental_of_vehicle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
