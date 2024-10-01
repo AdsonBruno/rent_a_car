a12b1a73fdbabf1d453bf5988ba7666ba143d286
AdsonBruno <adsonbruno2@gmail.com>
2024-10-01 19:31:52 -0300

    feat: add onboarding images and add colors in AppColors

---
+++ lib/views/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color green = Color(0xFF04662b);
+
+  static const Color lightGray = Color(0xFFD9D9D9);
+}
