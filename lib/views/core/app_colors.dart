855f96fadef9754f721df915236fd820ad89763e
AdsonBruno <adsonbruno2@gmail.com>
2024-10-09 23:13:20 -0300

    feat: add colors to button text and icons when selected and add icon assets

--- lib/views/core/app_colors.dart
+++ lib/views/core/app_colors.dart
@@ -5,6 +5,7 @@ class AppColors {
 
   static const Color green = Color(0xFF04662b);
   static const Color greenTextButton = Color(0xFF004B34);
-
+  static const Color darkLeafGreen = Color(0xFF015F2A);
+  static const Color greenSelected = Color(0xFF24FF00);
   static const Color lightGray = Color(0xFFD9D9D9);
 }
