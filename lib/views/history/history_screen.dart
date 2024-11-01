8045bd9f142c36cef402fc76ddb103de0ae5b457
AdsonBruno <adsonbruno2@gmail.com>
2024-11-01 18:06:34 -0300

    feat: add history screen

---
+++ lib/views/history/history_screen.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:rental_of_vehicle/views/core/app_colors.dart';
+import 'package:rental_of_vehicle/views/widgets/bottom_navigation_bar/bottom_navigation_bar_widget.dart';
+import 'package:rental_of_vehicle/views/widgets/card/base_card_widget.dart';
+
+class HistoryScreen extends StatelessWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            body: const SingleChildScrollView(
+              child: Column(
+                children: [
+                  Center(
+                    child: Text(
+                      'Minhas Locações',
+                      style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 14,
+                        color: AppColors.black,
+                      ),
+                    ),
+                  ),
+                  BaseCardWidget(
+                    title: 'Locação finalizada',
+                    children: [
+                      Text(
+                        'Devolução',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 9),
+                      Text(
+                        'AGÊNCIA CENTRO - ARAPIRACA - AL',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        '05/04/2024',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  BaseCardWidget(
+                    title: 'Locação finalizada',
+                    children: [
+                      Text(
+                        'Devolução',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 9),
+                      Text(
+                        'AGÊNCIA CENTRO - ARAPIRACA - AL',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        '05/04/2024',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  BaseCardWidget(
+                    title: 'Locação finalizada',
+                    children: [
+                      Text(
+                        'Devolução',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 9),
+                      Text(
+                        'AGÊNCIA CENTRO - ARAPIRACA - AL',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        '05/04/2024',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  BaseCardWidget(
+                    title: 'Locação finalizada',
+                    children: [
+                      Text(
+                        'Devolução',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 9),
+                      Text(
+                        'AGÊNCIA CENTRO - ARAPIRACA - AL',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        '05/04/2024',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+            bottomNavigationBar: BottomNavigationBarWidget(
+              currentIndex: 3,
+            )));
+  }
+}
