099e3da098a9834b889f2260f9fec13a784c34e0
AdsonBruno <adsonbruno2@gmail.com>
2025-03-10 00:20:19 -0300

    feat: add next button

--- lib/views/customer_registration/login_registration_screen.dart
+++ lib/views/customer_registration/login_registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:rental_of_vehicle/controllers/customer/login_registration_contro
 import 'package:rental_of_vehicle/models/customer_registration_models/customer_data_model.dart';
 import 'package:rental_of_vehicle/models/login_registration_models/login_validation_model.dart';
 import 'package:rental_of_vehicle/views/core/app_colors.dart';
+import 'package:rental_of_vehicle/views/widgets/button/button_widget.dart';
 import 'package:rental_of_vehicle/views/widgets/text_form_field_widget/custom_text_form_field_widget.dart';
 
 class LoginRegistrationScreen extends StatefulWidget {
@@ -15,12 +16,39 @@ class LoginRegistrationScreen extends StatefulWidget {
 
 class _LoginRegistrationScreenState extends State<LoginRegistrationScreen> {
   final controller = LoginRegistrationController();
+  final ValueNotifier<bool> isButtonEnable = ValueNotifier(false);
+  CustomerData? customerData;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.emailController.addListener(_updateButtonState);
+    controller.emailConfirmationController.addListener(_updateButtonState);
+    controller.passwordController.addListener(_updateButtonState);
+    controller.passwordConfirmationController.addListener(_updateButtonState);
+  }
+
+  @override
+  void dispose() {
+    controller.emailController.removeListener(_updateButtonState);
+    controller.emailConfirmationController.removeListener(_updateButtonState);
+    controller.passwordController.removeListener(_updateButtonState);
+    controller.passwordConfirmationController
+        .removeListener(_updateButtonState);
+  }
+
+  void _updateButtonState() {
+    final isValid = controller.emailController.text.isNotEmpty &&
+        controller.emailConfirmationController.text.isNotEmpty &&
+        controller.passwordController.text.isNotEmpty &&
+        controller.passwordConfirmationController.text.isNotEmpty;
+    isButtonEnable.value = isValid;
+  }
 
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)?.settings.arguments;
 
-    CustomerData? customerData;
     if (args is CustomerData) {
       customerData = args;
     }
@@ -76,6 +104,23 @@ class _LoginRegistrationScreenState extends State<LoginRegistrationScreen> {
                   validator: LoginRegistrationValidationModel
                       .validatePasswordConfirmation,
                 ),
+                const SizedBox(height: 206),
+                Center(
+                  child: ValueListenableBuilder<bool>(
+                      valueListenable: isButtonEnable,
+                      builder: (context, enable, child) {
+                        return ButtonWidget(
+                          nameButton: 'Próximo',
+                          enable: enable,
+                          onPressed: () {
+                            if (controller.formKey.currentState?.validate() ??
+                                false) {
+                              controller.validateForm();
+                            }
+                          },
+                        );
+                      }),
+                )
               ],
             ),
           ),
