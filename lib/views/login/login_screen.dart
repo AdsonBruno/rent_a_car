cc7272d9255aa3ea540df58585801a72cf37a89a
Adson Bruno <adson@disparopro.com.br>
2025-04-08 07:10:46 -0300

    feat: add ellipse format in login screen

--- lib/views/login/login_screen.dart
+++ lib/views/login/login_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:rental_of_vehicle/controllers/login_controller/login_controller.dart';
 import 'package:rental_of_vehicle/views/core/app_colors.dart';
 import 'package:rental_of_vehicle/views/widgets/button/button_widget.dart';
@@ -17,73 +21,91 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            reverse: true,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 19, top: 50, right: 19),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Olá!',
+      body: SizedBox.expand(
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              left: -MediaQuery.of(context).size.width * 0.6,
+              right: 140,
+              child: SvgPicture.asset(
+                'assets/others/ellipse.svg',
+                height: MediaQuery.of(context).size.height * 0.4,
+                // width: (MediaQuery.of(context).size.width - 30) / 3,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SingleChildScrollView(
+              reverse: true,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 19, top: 50, right: 19),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Olá!',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w900,
+                          fontFamily: 'Inter',
+                          color: AppColors.green,
+                        )),
+                    const Text(
+                      'Acesse sua conta ou cadastre-se',
                       style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w900,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
                         fontFamily: 'Inter',
-                        color: AppColors.green,
-                      )),
-                  const Text(
-                    'Acesse sua conta ou cadastre-se',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
-                      fontFamily: 'Inter',
-                    ),
-                  ),
-                  const SizedBox(height: 100),
-                  Stack(children: [
-                    Card(
-                      elevation: 7.0,
-                      color: AppColors.white, //AppColors.black,
-                      shadowColor: Colors.grey,
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
-                          children: [
-                            CustomTextFormFieldWidget(
-                              controller: controller.emailController,
-                              labelText: 'E-mail',
-                            ),
-                            const SizedBox(height: 30),
-                            CustomTextFormFieldWidget(
-                              controller: controller.passwordController,
-                              isPasswordField: true,
-                              labelText: 'Senha',
-                            ),
-                            const SizedBox(height: 56),
-                            ButtonWidget(nameButton: 'ENTRAR', onPressed: () {})
-                          ],
-                        ),
                       ),
                     ),
-                  ]),
-                ],
+                    const SizedBox(height: 100),
+                    Stack(children: [
+                      Card(
+                        elevation: 7.0,
+                        color: AppColors.white, //AppColors.black,
+                        shadowColor: Colors.grey,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              CustomTextFormFieldWidget(
+                                controller: controller.emailController,
+                                labelText: 'E-mail',
+                              ),
+                              const SizedBox(height: 30),
+                              CustomTextFormFieldWidget(
+                                controller: controller.passwordController,
+                                isPasswordField: true,
+                                labelText: 'Senha',
+                              ),
+                              const SizedBox(height: 56),
+                              ButtonWidget(
+                                nameButton: 'ENTRAR',
+                                onPressed: () {},
+                              ),
+                              const SizedBox(height: 20),
+                              TextButton(
+                                child: const Text(
+                                  'AINDA NÃO TENHO CADASTRO',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontFamily: 'Inter',
+                                    fontWeight: FontWeight.w600,
+                                    color: AppColors.darkLeafGreen,
+                                  ),
+                                ),
+                                onPressed: () {},
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ],
+                ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: ClipPath(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 2,
-                color: AppColors.green,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
