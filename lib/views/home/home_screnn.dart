8ab383a1f8957b5bb767fd9aa21a07fcabefca64
Adson Bruno <adson@disparopro.com.br>
2025-04-14 08:09:15 -0300

    feat: add bottom navigation bar in home screen

--- lib/views/home/home_screnn.dart
+++ lib/views/home/home_screnn.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:rental_of_vehicle/views/core/app_colors.dart';
+import 'package:rental_of_vehicle/views/widgets/bottom_navigation_bar/bottom_navigation_bar_widget.dart';
+import 'package:rental_of_vehicle/views/widgets/button/button_widget.dart';
+
+import '../../controllers/home_controller/home_controller.dart';
+import '../widgets/text_form_field_widget/custom_text_form_field_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +15,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final controller = HomeController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,23 +27,61 @@ class _HomeScreenState extends State<HomeScreen> {
             width: 500,
             color: AppColors.green,
           ),
-          const Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 20),
-              Center(
-                child: Text(
-                  'LOCA CAR UFAL',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800,
-                    fontFamily: 'inter',
-                    color: AppColors.white,
+          SingleChildScrollView(
+            // reverse: true,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                const Center(
+                  child: Text(
+                    'LOCA CAR UFAL',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      fontFamily: 'inter',
+                      color: AppColors.white,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          )
+                const SizedBox(height: 40),
+                Card(
+                  elevation: 7.0,
+                  color: AppColors.white,
+                  shadowColor: Colors.grey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        CustomTextFormFieldWidget(
+                          controller: controller.vehicleWithdrawalController,
+                          labelText: 'Onde deseja retirar o veículo?',
+                        ),
+                        CustomTextFormFieldWidget(
+                          controller: controller.returnVehicleController,
+                          labelText: 'Onde deseja devolver o veículo?',
+                        ),
+                        CustomTextFormFieldWidget(
+                          controller: controller.dateOfWithdrawalController,
+                          labelText: 'Data de retirada',
+                        ),
+                        CustomTextFormFieldWidget(
+                          controller: controller.returnDateController,
+                          labelText: 'Data de entrega',
+                        ),
+                        const SizedBox(height: 25),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ButtonWidget(nameButton: 'Continuar', onPressed: () {}),
+                BottomNavigationBarWidget(
+                  currentIndex: 2,
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
