f179d7291c12949185c11086dc9d65765c23f766
Adson Bruno <adson@disparopro.com.br>
2025-05-07 00:38:17 -0300

    refactor: move from Withdrawal confirmation to auxiliary widgets

---
+++ lib/views/core/app_text_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:rental_of_vehicle/views/core/app_colors.dart';
+
+class AppTextStyles {
+  static const TextStyle cardTitle = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'inter',
+    color: AppColors.black,
+  );
+
+  static const TextStyle informationText = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+    color: AppColors.black,
+  );
+}
--- lib/views/home/withdrawal_confirmation.dart
+++ lib/views/home/withdrawal_confirmation.dart
@@ -4,10 +4,13 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rental_of_vehicle/controllers/home_controller/withdrawal_confirmation_controller.dart';
 import 'package:rental_of_vehicle/views/core/app_colors.dart';
+import 'package:rental_of_vehicle/views/core/app_text_styles.dart';
 import 'package:rental_of_vehicle/views/widgets/button/button_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:rental_of_vehicle/views/widgets/card/attendant_features_card_widget.dart';
 
 import '../widgets/card/custom_card_widget.dart';
+import '../widgets/card/reservation_details_card_widget.dart';
 
 class WithdrawalConfirmation extends StatelessWidget {
   final Map<String, dynamic> vehicleData;
@@ -46,175 +49,15 @@ class WithdrawalConfirmation extends StatelessWidget {
                 padding: const EdgeInsets.only(left: 17, right: 17),
                 child: Column(
                   children: [
-                    CustomCardWidget(
-                      width: screenWidth * 0.9,
-                      height: screenHeight * 0.25,
-                      backgroundColor: AppColors.white,
-                      showBorder: true,
-                      borderWidth: 2,
-                      borderColor: AppColors.green,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            left: screenWidth * 0.00,
-                            top: screenHeight * 0.01,
-                            bottom: screenHeight * 0.02),
-                        child: Row(
-                          children: [
-                            Image.asset(
-                              'assets/icons/front_desk_icon.png',
-                              width: screenWidth * 0.12,
-                              height: screenHeight * 0.08,
-                            ),
-                            const SizedBox(width: 17),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text(
-                                    'Com atendente no balcão',
-                                    style: TextStyle(
-                                      fontFamily: 'Inter',
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.w300,
-                                      color: AppColors.black,
-                                    ),
-                                  ),
-                                  // SizedBox(height: 40),
-                                  SizedBox(height: screenHeight * 0.045),
-                                  const Text(
-                                    'Retire seu veículo com um de nosso atendentes',
-                                    style: TextStyle(
-                                      fontFamily: 'Inter',
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w200,
-                                      color: AppColors.black,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 8),
-                                  const Text(
-                                    'Escolha o modelo na hora da retirada',
-                                    style: TextStyle(
-                                      fontFamily: 'Inter',
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w200,
-                                      color: AppColors.black,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 8),
-                                  const Text(
-                                    'Agilidade na retirada',
-                                    style: TextStyle(
-                                      fontFamily: 'Inter',
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w200,
-                                      color: AppColors.black,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 8),
-                                  const Text(
-                                    '+200 pontos de fidelidade',
-                                    style: TextStyle(
-                                      fontFamily: 'Inter',
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w200,
-                                      color: AppColors.black,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 8),
-                                  const Text(
-                                    '50 pontos de milhas',
-                                    style: TextStyle(
-                                      fontFamily: 'Inter',
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w200,
-                                      color: AppColors.black,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 8),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    const AttendantFeaturesCardWidget(),
                     const SizedBox(height: 20),
-                    CustomCardWidget(
-                      width: screenWidth * 0.9,
-                      height: screenHeight * 0.25,
-                      backgroundColor: AppColors.white,
-                      showBorder: true,
-                      borderWidth: 2,
-                      borderColor: AppColors.green,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            left: 18, top: 5, right: 30, bottom: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Dados da reserva',
-                              style: TextStyle(
-                                fontFamily: 'Inter',
-                                fontSize: 17,
-                                fontWeight: FontWeight.w300,
-                                color: AppColors.black,
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            Text(
-                              'Nome do veículo: ${withdrawalController.title}',
-                              style: const TextStyle(
-                                fontFamily: 'Inter',
-                                fontSize: 12,
-                                fontWeight: FontWeight.w200,
-                                color: AppColors.black,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Local da retirada: ${withdrawalController.pickupLocation}',
-                              style: const TextStyle(
-                                fontFamily: 'Inter',
-                                fontSize: 12,
-                                fontWeight: FontWeight.w200,
-                                color: AppColors.black,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Local da devolução: ${withdrawalController.returnLocation}',
-                              style: const TextStyle(
-                                fontFamily: 'Inter',
-                                fontSize: 12,
-                                fontWeight: FontWeight.w200,
-                                color: AppColors.black,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Data da retirada: ${withdrawalController.pickupDateTime}',
-                              style: const TextStyle(
-                                fontFamily: 'Inter',
-                                fontSize: 12,
-                                fontWeight: FontWeight.w200,
-                                color: AppColors.black,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Data da devolução: ${withdrawalController.returnDateTime}',
-                              style: const TextStyle(
-                                fontFamily: 'Inter',
-                                fontSize: 12,
-                                fontWeight: FontWeight.w200,
-                                color: AppColors.black,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                          ],
-                        ),
-                      ),
+                    ReservationDetailsCardWidget(
+                      cardTitle: 'Dados da reserva',
+                      title: withdrawalController.title!,
+                      pickupLocation: withdrawalController.pickupLocation!,
+                      returnLocation: withdrawalController.returnLocation!,
+                      pickupDateTime: withdrawalController.pickupDateTime!,
+                      returnDateTime: withdrawalController.returnDateTime!,
                     ),
                   ],
                 ),
